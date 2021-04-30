5f48a2dd0115c8da865b9b7d8ab7031608dbc432
Rimshan Mohommad <mrimshan97@gmail.com>
2021-04-29 21:52:56 -0700

    Initial Commit

---
+++ CafeManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CafeManager
+//
+//  Created by Rimshan on 4/29/21.
+//  Copyright © 2021 Rimshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
